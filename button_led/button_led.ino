324ffcc2d5188998f8a1ad3dbf9afa55a3a09696
Alec Hewitt <ahewitt0000@gmail.com>
2014-12-26 21:25:09 -0500

    Added button_led and led_blink projects

---
+++ button_led/button_led.ino
@@ -0,0 +1,21 @@
+//toggles an LED when a button is pressed
+
+int LED = 12;
+int BUTTON = 4;
+
+void setup(){
+ 
+  pinMode(LED,OUTPUT); 
+ pinMode(BUTTON, INPUT);
+
+}
+
+void loop(){
+  
+  if(digitalRead(BUTTON) == HIGH){
+    digitalWrite(LED, HIGH);
+  } else {
+   digitalWrite(LED,LOW); 
+  }
+  
+}
---
+++ led_blink/led_blink.ino
@@ -0,0 +1,12 @@
+int LED = 12;
+
+void setup(){
+ pinMode(LED,OUTPUT); 
+}
+
+void loop(){
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED,LOW);
+  delay(1000);
+}
